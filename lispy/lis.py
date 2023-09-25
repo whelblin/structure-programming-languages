ceda3ab4b619ceddc89d38d8a94f16c718e7eb48
Greg DeLozier <gregdelozier@users.noreply.github.com>
2023-09-25 21:01:31 +0000

    lispy with procedures

--- lispy/lis.py
+++ lispy/lis.py
@@ -10,8 +10,8 @@ Number = (int, float)     # A Scheme Number is implemented as a Python int or fl
 Atom   = (Symbol, Number) # A Scheme Atom is a Symbol or Number
 List   = list             # A Scheme List is implemented as a Python list
 Exp    = (Atom, List)     # A Scheme expression is an Atom or List
-Env    = dict             # A Scheme environment (defined below) 
-                          # is a mapping of {variable: value}
+#Env    = dict             # A Scheme environment (defined below) 
+#                          # is a mapping of {variable: value}
 
 def parse(program: str) -> Exp:
     "Read a Scheme expression from a string."
@@ -40,6 +40,15 @@ def atom(token: str) -> Atom:
         try: return float(token)
         except ValueError:
             return Symbol(token)
+
+class Env(dict):
+    "An environment: a dict of {'var': val} pairs, with an outer Env."
+    def __init__(self, parms=(), args=(), outer=None):
+        self.update(zip(parms, args))
+        self.outer = outer
+    def find(self, var):
+        "Find the innermost Env where var appears."
+        return self if (var in self) else self.outer.find(var)
 
 def standard_env() -> Env:
     "An environment with some Scheme standard procedures."
@@ -76,23 +85,53 @@ def standard_env() -> Env:
 
 global_env = standard_env()
 
-def eval(x: Exp, env=global_env) -> Exp:
+def eval(x, env=global_env):
     "Evaluate an expression in an environment."
-    if isinstance(x, Symbol):        # variable reference
-        return env[x]
-    elif isinstance(x, Number):      # constant number
-        return x                
-    elif x[0] == 'if':               # conditional
-        (_, test, conseq, alt) = x
+    if isinstance(x, Symbol):    # variable reference
+        return env.find(x)[x]
+    elif not isinstance(x, List):# constant 
+        return x   
+    op, *args = x       
+    if op == 'quote':            # quotation
+        return args[0]
+    elif op == 'if':             # conditional
+        (test, conseq, alt) = args
         exp = (conseq if eval(test, env) else alt)
         return eval(exp, env)
-    elif x[0] == 'define':           # definition
-        (_, symbol, exp) = x
+    elif op == 'define':         # definition
+        (symbol, exp) = args
         env[symbol] = eval(exp, env)
-    else:                            # procedure call
-        proc = eval(x[0], env)
-        args = [eval(arg, env) for arg in x[1:]]
-        return proc(*args)
+    elif op == 'set!':           # assignment
+        (symbol, exp) = args
+        env.find(symbol)[symbol] = eval(exp, env)
+    elif op == 'lambda':         # procedure
+        (parms, body) = args
+        return Procedure(parms, body, env)
+    else:                        # procedure call
+        proc = eval(op, env)
+        vals = [eval(arg, env) for arg in args]
+        return proc(*vals)
+
+def schemestr(exp):
+    "Convert a Python object back into a Scheme-readable string."
+    if isinstance(exp, List):
+        return '(' + ' '.join(map(schemestr, exp)) + ')' 
+    else:
+        return str(exp)
+
+class Procedure(object):
+    "A user-defined Scheme procedure."
+    def __init__(self, parms, body, env):
+        self.parms, self.body, self.env = parms, body, env
+    def __call__(self, *args): 
+        return eval(self.body, Env(self.parms, args, self.env))
+
+def repl(prompt='lis.py> '):
+    "A prompt-read-eval-print loop."
+    while True:
+        val = eval(parse(input(prompt)))
+        if val is not None: 
+            print(schemestr(val))
 
 def test_tokenize():
     program = "(begin (define r 10) (* pi (* r r)))"
@@ -110,9 +149,16 @@ def test_eval():
     program = "(begin (define r 10) (* pi (* r r)))"
     assert(eval(parse(program))) == 314.1592653589793
 
+def test_schemestr():
+    expr = ['begin', ['define', 'r', 10], ['*', 'pi', ['*', 'r', 'r']]]
+    assert schemestr(expr) == "(begin (define r 10) (* pi (* r r)))"
+
+
 if __name__ == "__main__":
     test_tokenize()
     test_parse()
     test_global_env()
     test_eval()
+    test_schemestr()
     print("done")
+    repl()
